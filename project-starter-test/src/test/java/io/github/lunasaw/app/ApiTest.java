1ca26ef301652dd2a6a698682a84ab68b363aebb
luna <52351139+lunasaw@users.noreply.github.com>
2025-12-16 11:07:21 +0800

    Initial commit

---
+++ project-starter-test/src/test/java/io/github/lunasaw/app/ApiTest.java
@@ -0,0 +1,23 @@
+package io.github.lunasaw.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author luna
+ * @version 1.0
+ * @date 2023/12/3
+ * @description:
+ */
+@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+public class ApiTest {
+
+    @test
+    public void atest() {
+
+    }
+
+}
